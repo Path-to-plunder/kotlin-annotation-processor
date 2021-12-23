0a2456d10f30a8b64dfe1f3d7c9c31b6d08959cb
Gabriel Spencer <konk3r@gmail.com>
2021-12-23 15:27:58 -0800

    Move annotation processing setup into AnnotationParser

--- src/main/kotlin/com/casadetasha/kexp/annotationparser/AnnotationParser.kt
+++ src/main/kotlin/com/casadetasha/kexp/annotationparser/AnnotationParser.kt
@@ -1,19 +1,80 @@
 package com.casadetasha.kexp.annotationparser
 
+import com.casadetasha.kexp.annotationparser.kxt.*
+import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
+import javax.annotation.processing.Messager
 import javax.annotation.processing.ProcessingEnvironment
+import javax.annotation.processing.RoundEnvironment
+import javax.tools.Diagnostic
+import kotlin.reflect.KClass
 
 object AnnotationParser {
+
+    const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
+
     private var processingEnv_: ProcessingEnvironment? = null
-    internal var processingEnv: ProcessingEnvironment
+    var processingEnv: ProcessingEnvironment
         get() {
-            if (processingEnv_ == null) throw IllegalStateException("AnnotationParser.setup(ProcessingEnvironment) must be called before using annotation parser extensions.")
+            if (processingEnv_ == null) throwNotSetupException()
             return processingEnv_!!
         }
         set(processingEnv) {
             processingEnv_ = processingEnv
         }
 
-    fun setup(processingEnv: ProcessingEnvironment) {
+    private var kaptKotlinGeneratedDir_: String? = null
+    val kaptKotlinGeneratedDir: String
+        get() {
+            if (kaptKotlinGeneratedDir_ == null) throwNotSetupException()
+            return kaptKotlinGeneratedDir_!!
+        }
+
+    private val messager: Messager by lazy { processingEnv.messager }
+    private lateinit var roundEnv: RoundEnvironment
+
+    fun setup(processingEnv: ProcessingEnvironment, roundEnv: RoundEnvironment?) {
+        val incomingKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]
+        if (incomingKotlinGeneratedDir == null || roundEnv == null) {
+            printThenThrowError(
+                "roundEnvironment and kapt generated dir for ProcessingEnvironment must both be non null")
+        }
+
         this.processingEnv = processingEnv
+        this.roundEnv = roundEnv
+        kaptKotlinGeneratedDir_ = incomingKotlinGeneratedDir
     }
+
+    fun printNote(text: String) {
+        messager.printMessage(Diagnostic.Kind.NOTE, text)
+    }
+
+    fun printThenThrowError(errorMessage: String): Nothing {
+        messager.printMessage(Diagnostic.Kind.ERROR, errorMessage)
+        throw IllegalArgumentException(errorMessage)
+    }
+
+    @OptIn(KotlinPoetMetadataPreview::class)
+    fun getClassesAnnotatedWith(
+        annotationClass: KClass<out Annotation>
+    ): Set<KotlinContainer.KotlinClass> = roundEnv.getElementsAnnotatedWith(annotationClass.java)
+        .filter { it.isClass() }
+        .map {
+            val className = it.getClassName()
+            KotlinContainer.KotlinClass(
+                element = it,
+                className = className,
+                classData = className.getClassData(),
+                functionElementMap = it.getChildFunctionElementMap()
+            )
+        }.toSet()
+
+    @OptIn(KotlinPoetMetadataPreview::class)
+    fun getFileFacadesForTopLevelFunctionsAnnotatedWith(
+        annotations: List<KClass<out Annotation>>
+    ): Set<KotlinContainer.KotlinFileFacade> {
+        return FileFacadeParser(roundEnv).getFacadesForFilesContainingAnnotations(annotations)
+    }
+
+    private fun throwNotSetupException(): Nothing = printThenThrowError(
+        "setup must be called before using AnnotationParser.")
 }
--- src/main/kotlin/com/casadetasha/kexp/annotationparser/kxt/FileFacadeParser.kt
+++ src/main/kotlin/com/casadetasha/kexp/annotationparser/kxt/FileFacadeParser.kt
@@ -7,7 +7,7 @@ import javax.lang.model.element.Element
 import kotlin.reflect.KClass
 
 @OptIn(KotlinPoetMetadataPreview::class)
-class FileFacadeParser(private val roundEnvironment: RoundEnvironment) {
+internal class FileFacadeParser(private val roundEnvironment: RoundEnvironment) {
     private val functionListMap = HashMap<String, MutableList<Element>>()
     private val functionFileElementMap = HashMap<String, Element>()
 
